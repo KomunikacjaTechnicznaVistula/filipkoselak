8adb4e67588219f3986e74a4b8a4219068025691
Daniel Barrio Fierro <dbarr@dolby.com>
2022-05-05 15:16:34 +0200

    1st commit

---
+++ docs/oxygen-webhelp/app/nav-links/json/c_settings-d176e112.js
@@ -0,0 +1,1 @@
+define({"topics":[{"title":"How to change the settings","shortdesc":"<p class=\"shortdesc\">Changing the settings<\/p>","href":"topics\/c_settings.html#t_settings","attributes":{"data-id":"t_settings"},"menu":{"hasChildren":false},"tocID":"t_settings-d176e125","topics":[]}]});
---
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,13 @@
+define(function() {var keywords=[{w:"***",p:["p0"]},{w:"Installing",p:["p1"]},{w:"the",p:["p1"]},{w:"application",p:["p1"]},{w:"Introduction",p:["p2"]},{w:"Learning",p:["p3"]},{w:"with",p:["p3"]},{w:"Mimo",p:["p3"]},{w:"Elements",p:["p4"]},{w:"Settings",p:["p5"]}];
+var ph={};
+ph["p0"]=[0];
+ph["p1"]=[1, 2, 3];
+ph["p2"]=[4];
+ph["p3"]=[5, 6, 7];
+ph["p4"]=[8];
+ph["p5"]=[9];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
